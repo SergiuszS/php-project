80955dc5812602a00c429fad9c6ea60338845339
Sergiusz <userjscpp@gmail.com>
2017-09-04 11:17:41 +0200

    Basic settings and twig template

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 php-project
 ===========
 
-Object-oriented login and registration system (in Symfony)
-A Symfony project created on September 1, 2017, 7:51 pm.
+Object-oriented login and registration system (in Symfony) created on September 1, 2017, 7:51 pm.
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,15 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name = "homepage")
      */
-    public function indexAction(Request $request)
+    public function defaultAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        
+        return $this->render('default/index.html.twig', array(
+            'test' => 'test')
+        );
     }
+
+
 }
